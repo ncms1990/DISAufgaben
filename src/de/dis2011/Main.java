66cab478a438970d6e1b520a7f9711b5a5603e63
Nikki <notARe@lemail.com>
2018-05-15 01:53:47 +0200

    Up until here starting with Task nr.3.

--- src/de/dis2011/Main.java
+++ src/de/dis2011/Main.java
@@ -21,7 +21,6 @@ public class Main extends Application {
 		launch(args);
     }
 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
@@ -29,76 +28,4 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-	//
-	// EVERYTHING DOWN HERE IS FROM EXAMPLE
-	//
-	/**
-	 * Zeigt das Hauptmenü
-	 */
-	public static void showMainMenu() {
-		//Menüoptionen
-		final int MENU_MAKLER = 0;
-		final int QUIT = 1;
-		
-		//Erzeuge Menü
-		Menu mainMenu = new Menu("Hauptmenü");
-		mainMenu.addEntry("EstateAgent-Verwaltung", MENU_MAKLER);
-		mainMenu.addEntry("Beenden", QUIT);
-		
-		//Verarbeite Eingabe
-		while(true) {
-			int response = mainMenu.show();
-			
-			switch(response) {
-				case MENU_MAKLER:
-					showMaklerMenu();
-					break;
-				case QUIT:
-					return;
-			}
-		}
-	}
-	
-	/**
-	 * Zeigt die Maklerverwaltung
-	 */
-	public static void showMaklerMenu() {
-		//Menüoptionen
-		final int NEW_MAKLER = 0;
-		final int BACK = 1;
-		
-		//Maklerverwaltungsmenü
-		Menu maklerMenu = new Menu("EstateAgent-Verwaltung");
-		maklerMenu.addEntry("Neuer EstateAgent", NEW_MAKLER);
-		maklerMenu.addEntry("Zurück zum Hauptmenü", BACK);
-		
-		//Verarbeite Eingabe
-		while(true) {
-			int response = maklerMenu.show();
-			
-			switch(response) {
-				case NEW_MAKLER:
-					newMakler();
-					break;
-				case BACK:
-					return;
-			}
-		}
-	}
-	
-	/**
-	 * Legt einen neuen EstateAgent an, nachdem der Benutzer
-	 * die entprechenden Daten eingegeben hat.
-	 */
-	public static void newMakler() {
-		EstateAgent m = new EstateAgent();
-		
-		m.setName(FormUtil.readString("Name"));
-		m.setAddress(FormUtil.readString("Adresse"));
-		m.setLogin(FormUtil.readString("Login"));
-		m.setPassword(FormUtil.readString("Passwort"));
-		m.save();
-		
-		System.out.println("EstateAgent mit der ID "+m.getId()+" wurde erzeugt.");
-	}
 }
